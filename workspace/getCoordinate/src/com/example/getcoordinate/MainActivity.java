a58b96306601d6aaf4df9503873fdd317986c404
uinchester <uinchester@me.com>
2013-12-04 19:39:30 +0900

    音声認識による操作完成版

--- workspace/getCoordinate/src/com/example/getcoordinate/MainActivity.java
+++ workspace/getCoordinate/src/com/example/getcoordinate/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.getcoordinate;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import com.example.getcoordinate.R.id;
-
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
@@ -34,164 +34,12 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 public class MainActivity extends Activity implements OnClickListener,
 		SensorEventListener {
 	private static final int REQUEST_CODE = 0;
-	public static final String LOGTAG = null;
 	private SensorManager manager;
 	private TextView values;
-	private float acX, acY, acZ = 0; // 加速度センサーの値を受け取る変数class SpeechListener
-										// implements RecognitionListener {
-	private TextView textView1;
-
-	class SpeechListener implements RecognitionListener {
-
-		// 音声認識準備完了
-		public void onReadyForSpeech(Bundle params) {
-			Toast.makeText(getApplicationContext(), "音声認識準備完了",
-					Toast.LENGTH_SHORT);
-		}
-
-		// 音声入力開始
-
-		public void onBeginningOfSpeech() {
-			Toast.makeText(getApplicationContext(), "入力開始", Toast.LENGTH_SHORT);
-		}
-
-		// 録音データのフィードバック用
-		public void onBufferReceived(byte[] buffer) {
-
-		}
-
-		// 入力音声のdBが変化した
-		public void onRmsChanged(float rmsdB) {
-
-		}
-
-		// 音声入力終了
-		public void onEndOfSpeech() {
-			Toast.makeText(getApplicationContext(), "入力終了", Toast.LENGTH_SHORT);
-		}
-
-		// ネットワークエラー又は、音声認識エラー
-		public void onError(int error) {
-			switch (error) {
-			case SpeechRecognizer.ERROR_AUDIO:
-				// 音声データ保存失敗
-				break;
-			case SpeechRecognizer.ERROR_CLIENT:
-				// Android端末内のエラー(その他)
-				break;
-			case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-				// 権限無し
-				break;
-			case SpeechRecognizer.ERROR_NETWORK:
-				// ネットワークエラー(その他)
-				Log.e(LOGTAG, "network error");
-				break;
-			case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-				// ネットワークタイムアウトエラー
-				Log.e(LOGTAG, "network timeout");
-				break;
-			case SpeechRecognizer.ERROR_NO_MATCH:
-				// 音声認識結果無し
-				Toast.makeText(getApplicationContext(), "no match Text data",
-						Toast.LENGTH_LONG);
-				break;
-			case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-				// RecognitionServiceへ要求出せず
-				break;
-			case SpeechRecognizer.ERROR_SERVER:
-				// Server側からエラー通知
-				break;
-			case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-				// 音声入力無し
-				Toast.makeText(getApplicationContext(), "no input?",
-						Toast.LENGTH_LONG);
-				break;
-			default:
-			}
-		}
-
-		// イベント発生時に呼び出される
-		public void onEvent(int eventType, Bundle params) {
-			Log.v(LOGTAG, "onEvent");
-		}
-
-		// 部分的な認識結果が得られる場合に呼び出される
-		public void onPartialResults(Bundle partialResults) {
-			Log.v(LOGTAG, "onPartialResults");
-		}
-
-		// 認識結果
-		public void onResults(Bundle results) {
-			ArrayList<String> recData = results
-					.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-
-			String getData = new String();
-			for (String s : recData) {
-				getData += s + ",";
-			}
-			if ("上".equals(getData)) {
-				Toast.makeText(getApplicationContext(), getData,
-						Toast.LENGTH_LONG).show();
-				vcZ += 10;
-				if (vcZ >= 160) {
-					vcZ = 160;
-				}
-
-			} else if ("下".equals(getData)) {
-				Toast.makeText(getApplicationContext(), getData,
-						Toast.LENGTH_LONG).show();
-				vcZ -= 10;
-				if (vcZ <= 60) {
-					vcZ = 60;
-				}
-			} else if ("左".equals(getData)) {
-				Toast.makeText(getApplicationContext(), getData,
-						Toast.LENGTH_LONG).show();
-				vcX += 10;
-				if (vcX >= 56) {
-					vcX = 56;
-				}
-			} else if ("右".equals(getData)) {
-				Toast.makeText(getApplicationContext(), getData,
-						Toast.LENGTH_LONG).show();
-				if (vcX <= -56) {
-					vcX = -56;
-				}
-				vcX -= 10;
-			} else if ("前".equals(getData)) {
-				vcY += 10;
-				if (vcY >= 140) {
-					vcY = 140;
-				}
-			} else if ("後ろ".equals(getData)) {
-				Toast.makeText(getApplicationContext(), getData,
-						Toast.LENGTH_LONG).show();
-				vcY -= 10;
-				if (vcY <= 40) {
-					vcY = 40;
-				}
-
-			} else {
-				Toast.makeText(getApplicationContext(),
-						"もう一度お願いします。\n認識できる単語は「上」「下」「左」「右」「前」「後」です。",
-						Toast.LENGTH_LONG).show();
-			}
-			Toast.makeText(getApplicationContext(), getData, Toast.LENGTH_SHORT)
-					.show();
-		}
-
-	}
+	private float acX, acY, acZ = 0; // 加速度センサーの値を受け取る変数
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -200,32 +48,12 @@ public class MainActivity extends Activity implements OnClickListener,
 		// ver3.0から追加されたStrictModeに対するエラーの対処
 		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
 				.permitAll().build());
-		Button button3 = (Button) findViewById(R.id.button3);
-		TextView textView1 = (TextView) findViewById(R.id.text3);
 
-		button3.setOnClickListener(new View.OnClickListener() {
-
-			public void onClick(View arg0) {
-				// TODO 自動生成されたメソッド・スタブ
-				SpeechRecognizer sr = SpeechRecognizer
-						.createSpeechRecognizer(getApplicationContext());
-				sr.setRecognitionListener(new SpeechListener());
-				getApplicationContext();
-				Intent intent = new Intent(
-						RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-				intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-						RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-				intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
-						getPackageName());
-				intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
-				sr.startListening(intent);
-			}
-
-		});
 		/* ボタンの実装 */
 		// ボタンの取得
 		Button button1 = (Button) findViewById(R.id.button1); // 上昇ボタン
 		Button button2 = (Button) findViewById(R.id.button2); // 下降ボタン
+		Button button3 = (Button) findViewById(R.id.button3); // 音声認識ボタン
 		Button button4 = (Button) findViewById(R.id.button4); // 加速度センサーのON/OFF切り替え用ボタン
 		// 加速度センサー
 		values = (TextView) findViewById(R.id.value_id);
@@ -234,15 +62,39 @@ public class MainActivity extends Activity implements OnClickListener,
 		button1.setOnClickListener(this);
 		button2.setOnClickListener(this);
 		button4.setOnClickListener(this);
+		button3.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				try {
+					// インテント作成
+					Intent intent = new Intent(
+							RecognizerIntent.ACTION_RECOGNIZE_SPEECH); // ACTION_WEB_SEARCH
+					intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+							RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+					intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+							"「上」「下」「左」「右」「前」「後ろ」「つかむ」「はなす」のうち一つを発声してください。\nまた、「1」「2」「3」「4」「5」「6」により特定の位置にボールを運ぶことができます。"); // 音声認識時に表示する文字
+					intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1); // 返す候補を１つにする
+					// インテント発行
+					startActivityForResult(intent, REQUEST_CODE);
+				} catch (ActivityNotFoundException e) {
+					// このインテントに応答できるアクティビティがインストールされていない場合(エミュレータでの実行時に発生する例外処理)
+					Toast.makeText(MainActivity.this,
+							"ActivityNotFoundException", Toast.LENGTH_LONG)
+							.show();
+				}
+			}
+		});
+
 	}
 
 	int i = 0;
+	int j = 0;
+	int k = 0;
 	int x = 0;
 	int y = 0;
 	float tempX = 0;
 	float tempY = 0;
-	int tempZ = 140;
-	int z = 140;
+	int tempZ = 160;
+	int z = 160;
 	float previousDistance = 0;
 	// タッチした時の時間
 	long time = 0;
@@ -255,17 +107,19 @@ public class MainActivity extends Activity implements OnClickListener,
 	int sendX = 0;
 	int sendY = 0;
 	int sendZ = 0;
-	int acmove = 0;
+	int acmove = 1;
 	float count = 0;
-	int vcX = x;
-	int vcY = y;
-	int vcZ = z;
+	int vcX = 0;
+	int vcY = 0;
+	int vcZ = 0;
+	int mode = 0;
+	int voiceI = 0;
+	String voiceJ = null;
+	String textJ = null;
 
 	// スクリーンがタッチされたかどうかの判定
 	public boolean onTouchEvent(MotionEvent event) {
 		move(event);
-		int a = event.getActionMasked();
-		Log.v("a", new Integer(a).toString());
 		int mx = y, mz = 0;
 		String[] strs = trans(mx, mz);
 		String actionX = strs[0];
@@ -331,7 +185,6 @@ public class MainActivity extends Activity implements OnClickListener,
 				acmove = 0;
 			}
 		}
-
 	}
 
 	@SuppressLint("FloatMath")
@@ -357,7 +210,6 @@ public class MainActivity extends Activity implements OnClickListener,
 			}
 		}
 		y += 90;
-
 		// }
 		// マルチタッチ
 		/*
@@ -418,10 +270,16 @@ public class MainActivity extends Activity implements OnClickListener,
 			String action;
 			if (i == -1) {
 				action = "exit";
-			} else {
+			} else if (i == 7) {
+				action = "move1";// つかむ
+			} else if (i == 8) {
+				action = "move2";// はなす
+			} else if (i == 0) {
 				action = actionX + ", " + actionY + ", " + actionZ + ", -180, "
 						+ i;
-			}
+			} else {
+				action = Integer.toString(i);// 1から6までの位置へ移動する命令を送る
+			} 
 			pw.println(action);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
@@ -521,15 +379,23 @@ public class MainActivity extends Activity implements OnClickListener,
 				// ここでは、文字列が複数あった場合に結合しています
 				resultsString += results.get(i);
 			}
-			// 以外の入力のみ結果を表示する
 
+			// 以外の入力のみ結果を表示する
+			String patternString = "[1-6]+"; // 1から6のどれかひとつ以上
+			Pattern p = Pattern.compile(patternString);
+			Matcher m = p.matcher(resultsString);
+			String[] hikaku = resultsString.split("");
+
+			vcX = y;
+			vcY = x;
+			vcZ = z;
+			Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
 			if ("上".equals(resultsString)) {
 				Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
 				vcZ += 10;
 				if (vcZ >= 160) {
 					vcZ = 160;
 				}
-
 			} else if ("下".equals(resultsString)) {
 				Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
 				vcZ -= 10;
@@ -538,37 +404,92 @@ public class MainActivity extends Activity implements OnClickListener,
 				}
 			} else if ("左".equals(resultsString)) {
 				Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
-				vcX += 10;
-				if (vcX >= 56) {
-					vcX = 56;
+				vcY += 10;
+				if (vcY >= 56) {
+					vcY = 56;
 				}
 			} else if ("右".equals(resultsString)) {
 				Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
-				if (vcX <= -56) {
-					vcX = -56;
+				vcY -= 10;
+				if (vcY <= -56) {
+					vcY = -56;
 				}
-				vcX -= 10;
 			} else if ("前".equals(resultsString)) {
-				vcY += 10;
-				if (vcY >= 140) {
-					vcY = 140;
+				Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
+				vcX += 10;
+				if (vcX >= 140) {
+					vcX = 140;
 				}
 			} else if ("後ろ".equals(resultsString)) {
 				Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
-				vcY -= 10;
-				if (vcY <= 40) {
-					vcY = 40;
+				vcX -= 10;
+				if (vcX <= 40) {
+					vcX = 40;
+				}
+			} else if ("つかむ".equals(resultsString)) {
+				Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
+				i = 7;
+				connect(null, null, null);
+				i = 0;
+				mode = 1;
+			} else if ("話す".equals(resultsString)) {
+				Toast.makeText(this, "はなす", Toast.LENGTH_LONG).show();
+				i = 8;
+				connect(null, null, null);
+				i = 0;
+				mode = 1;
+			} else if (m.find() == true) {// 1から6のどれかに移動する命令の時
+				for (j = 0; j < hikaku.length; j++) {
+					if (voiceI < 1) {
+						for (k = 1; k < 7; k++) {
+							voiceJ = Integer.toString(k);
+							if (voiceJ.equals(hikaku[j])) {// 数字の判定
+								if (voiceI == 0) { //一回目の認識
+									
+									Toast.makeText(this, "voice1",
+											Toast.LENGTH_SHORT).show();
+									textJ = hikaku[j];
+									i = Integer.parseInt(hikaku[j]);
+									connect(null, null, null);
+									i = 0;
+									voiceI++;
+
+								} else {
+									break;
+								}
+							}
+						}
+					} else {
+						break;
+					}
 				}
 
+				Toast.makeText(this, "現在地から" + textJ + "にボールを移動します。",
+						Toast.LENGTH_LONG).show();
+				textJ = null;
+				voiceI = 0;
+				mode = 1;
 			} else {
 				Toast.makeText(this,
-						"もう一度お願いします。\n認識できる単語は「上」「下」「左」「右」「前」「後」です。",
+						"もう一度お願いします。\n認識できる単語は「上」「下」「左」「右」「前」「後」「つかむ」「はなす」です。\nまた、「1」「2」「3」「4」「5」「6」により特定の位置にボールを運ぶことができます。",
 						Toast.LENGTH_LONG).show();
+				mode = 1;
 			}
+			if (mode == 0) {
+				String voiceX = Integer.toString(vcX);
+				String voiceY = Integer.toString(vcY);
+				String voiceZ = Integer.toString(vcZ);
+				connect(voiceX, voiceY, voiceZ);
+				x = vcY;
+				y = vcX;
+				z = vcZ;
+			}
+			mode = 0;
 		}
 
 		super.onActivityResult(requestCode, resultCode, data);
 	}
+
 	@Override
 	protected void onStop() {
 		// TODO 自動生成されたメソッド・スタブ
@@ -596,78 +517,80 @@ public class MainActivity extends Activity implements OnClickListener,
 
 	public void onSensorChanged(SensorEvent event) {
 		// TODO 自動生成されたメソッド・スタブ
+		if (acmove == 0) {
+			if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+				acX = event.values[0]; // 加速度センサー値のフィルタ処理
+				acY = event.values[1];
+				acZ = event.values[2];
+				SacX = SacX + acX;
+				SacY = SacY + acY;
+				count = +1;
+				// 現在の時間
+				long nowAC = System.currentTimeMillis();
+				// タッチした時間と現在の時間を比べる(ミリ秒)
+				if ((nowAC - time) > 250) {
+					SacX = SacX / count;
+					SacY = SacY / count;
+					if (SacX < 1) {
+						sendX = 140;
+					} else if (SacX < 2) {
+						sendX = 118;
+					} else if (SacX < 3) {
+						sendX = 110;
+					} else if (SacX < 4) {
+						sendX = 102;
+					} else if (SacX < 5) {
+						sendX = 94;
+					} else if (SacX < 6) {
+						sendX = 86;
+					} else if (SacX < 7) {
+						sendX = 78;
+					} else if (SacX < 8) {
+						sendX = 70;
+					} else if (SacX < 9) {
+						sendX = 60;
+					}
 
-		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			acX = event.values[0]; // 加速度センサー値のフィルタ処理
-			acY = event.values[1];
-			acZ = event.values[2];
-			SacX = SacX + acX;
-			SacY = SacY + acY;
-			count = +1;
-			// 現在の時間
-			long nowAC = System.currentTimeMillis();
-			// タッチした時間と現在の時間を比べる(ミリ秒)
-			if ((nowAC - time) > 250) {
-				SacX = SacX / count;
-				SacY = SacY / count;
-				if (SacX < 1) {
-					sendX = 140;
-				} else if (SacX < 2) {
-					sendX = 118;
-				} else if (SacX < 3) {
-					sendX = 110;
-				} else if (SacX < 4) {
-					sendX = 102;
-				} else if (SacX < 5) {
-					sendX = 94;
-				} else if (SacX < 6) {
-					sendX = 86;
-				} else if (SacX < 7) {
-					sendX = 78;
-				} else if (SacX < 8) {
-					sendX = 70;
-				} else if (SacX < 9) {
-					sendX = 32;
-				}
-
-				if (SacY < -5) {
-					sendY = 56;
-				} else if (SacY < -4) {
-					sendY = 47;
-				} else if (SacY < -3) {
-					sendY = 38;
-				} else if (SacY < -2) {
-					sendY = 29;
-				} else if (SacY < -1) {
-					sendY = 20;
-				} else if (SacY < 0) {
-					sendY = 11;
-				} else if (SacY < 1) {
-					sendY = 2;
-				} else if (SacY < 2) {
-					sendY = -10;
-				} else if (SacY < 3) {
-					sendY = -22;
-				} else if (SacY < 4) {
-					sendY = -34;
-				} else if (SacY < 5) {
-					sendY = -56;
-				}
-				String X = Integer.toString(sendX);
-				String Y = Integer.toString(sendY);
-				String Z = Integer.toString(160);
-				if (acmove == 0) { // 加速度センサーによる操作の可・不可切り替え
+					if (SacY < -5) {
+						sendY = 56;
+					} else if (SacY < -4) {
+						sendY = 47;
+					} else if (SacY < -3) {
+						sendY = 38;
+					} else if (SacY < -2) {
+						sendY = 29;
+					} else if (SacY < -1) {
+						sendY = 20;
+					} else if (SacY < 0) {
+						sendY = 11;
+					} else if (SacY < 1) {
+						sendY = 2;
+					} else if (SacY < 2) {
+						sendY = -10;
+					} else if (SacY < 3) {
+						sendY = -22;
+					} else if (SacY < 4) {
+						sendY = -34;
+					} else if (SacY < 5) {
+						sendY = -56;
+					}
+					x = sendY;
+					y = sendX;
+					z = 160;
+					String X = Integer.toString(sendX);
+					String Y = Integer.toString(sendY);
+					String Z = Integer.toString(160);
+					// 加速度センサーによる操作の可・不可切り替え
 					connect(X, Y, Z);
 					String str = "加速度センサー値:" + "\nX軸:" + acX + "\nY軸:" + acY;
 					values.setText(str);
+
+					SacX = 0;
+					SacY = 0;
+					count = 0;
+					time = nowAC;
 				}
-				SacX = 0;
-				SacY = 0;
-				count = 0;
-				time = nowAC;
 			}
-
 		}
-
 	}
 }
